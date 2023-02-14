f96752d2aa8d39515c555d201734445a3be85810
Stolbin <109080579+Stolbin@users.noreply.github.com>
2023-02-14 10:35:00 +0200

    Update Phonebook.jsx

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -14,12 +14,12 @@ class Phonebook extends Component {
 
   handleSubmit = e => {
     e.preventDefault();
-
-    this.props.onSubmit(this.state.name, this.state.number);
-
+    this.props.onSubmit(this.state);
     this.setState({ name: '', number: '' });
   };
+
   render() {
+    const { name, number } = this.state;
     return (
       <Form onSubmit={this.handleSubmit}>
         <Label>
@@ -27,6 +27,7 @@ class Phonebook extends Component {
           <Input
             type="text"
             name="name"
+            value={name}
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
@@ -37,7 +38,8 @@ class Phonebook extends Component {
           Number:
           <Input
             type="tel"
-            number="number"
+            name="number"
+            value={number}
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
